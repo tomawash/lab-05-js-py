eea207ef14f87e85e51381cdd2bccdf150472bdd
Tomasz Wiercioch <47215529+tomawash@users.noreply.github.com>
2022-03-07 16:48:37 -0500

    Madlibs files

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+noun = input("Enter a noun")
+adjective = input("Enter an adjective")
+adverb = input("Enter an adverb")
+verb = input("Enter a verb")
+
+print("On a tropical " + str(noun) + ", Underneath a " + str(adjective) + " moon. " + str(verb) + " with the hula dancers, " + str(adverb) + " askin' questions cause they got all the answers.");
